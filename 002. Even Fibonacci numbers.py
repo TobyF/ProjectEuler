9a4e7d1b61c130de1fdb6bb4deae1740686b1641
Toby Freeland <tobyfreeland@googlemail.com>
2016-03-10 09:14:32 +0000

    Updated 1 number

--- 002. Even Fibonacci numbers.py
+++ 002. Even Fibonacci numbers.py
@@ -1,5 +1,5 @@
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million,
-#find the sum of the even-valued terms.
+# find the sum of the even-valued terms.
 
 def fib(n, a=1, b=2):  #n0 = 0 and n1 = 1
     #A function that returns all the fibbonaci numbers to the term that goes over 4,000,000
